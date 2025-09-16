9b9cca5815fbcf5ad118b5a7b73ed96cf84b3f61
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2025-09-16 10:47:59 -0700

    Enable epansion of more content types and methods

--- src/middleware/ExpandInputs.php
+++ src/middleware/ExpandInputs.php
@@ -3,6 +3,7 @@
 namespace Hiraeth\Middleware;
 
 use Hiraeth;
+use Hiraeth\Http;
 
 use Psr\Http\Server\MiddlewareInterface as Middleware;
 use Psr\Http\Server\RequestHandlerInterface as Handler;
@@ -10,6 +11,7 @@ use Psr\Http\Server\RequestHandlerInterface as Handler;
 use Psr\Http\Message\UploadedFileInterface as UploadedFile;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use RuntimeException;
 
 /**
  *
@@ -21,33 +23,108 @@ class ExpandInputs implements Middleware
 	 */
 	public function process(Request $request, Handler $handler): Response
 	{
-		$type    = $request->getHeaderLine('Content-Type');
+		$type    = $request->getHeaderLine('content-type');
 		$request = $request->withQueryParams(
 			$this->expand($request, $request->getQueryParams())
 		);
 
-		if (in_array('multipart/form-data', explode(';', $type))) {
-			$inputs = [
-				'files' => 'UploadedFiles',
-				'body'  => 'ParsedBody'
-			];
-
-			foreach ($inputs as $method) {
-				$request = $request->{'with' . $method}(
-					$this->expand($request, $request->{'get' . $method}())
-				);
-			}
-		}
+		$request = match (TRUE) {
+			str_contains($type, 'application/x-www-form-urlencoded')
+				=> $this->processUrlEncoded($request),
+			str_contains($type, 'multipart/form-data')
+				=> $this->processMultipart($request),
+			str_contains($type, 'application/json')
+				=> $this->processJson($request),
+			default
+				=> $request
+		};
 
 		return $handler->handle($request);
 	}
 
 
 	/**
+	 *
+	 */
+	public function processJson(Request $request): Request
+	{
+		if (in_array($request->getMethod(), ['POST', 'PUT', 'PATCH', 'DELETE'])) {
+			$parsed = json_decode($request->getBody()->getContents(), TRUE);
+
+			if (is_array($parsed)) {
+				$request = $request->withParsedBody($parsed);
+			}
+		}
+
+		return $request;
+	}
+
+
+	/**
+	 *
+	 */
+	public function processMultipart(Request $request): Request
+	{
+		if (in_array($request->getMethod(), ['PUT', 'PATCH', 'DELETE'])) {
+			if (function_exists('request_parse_body')) {
+				$parsed  = request_parse_body();
+				$request = $request->withParsedBody($this->expand($parsed[0]));
+
+			} else {
+				throw new RuntimeException(sprintf(
+					'Method "%s" does not support type "%s" request on PHP < 8.4',
+					$request->getMethod(),
+					$request->getHeaderLine('content-type')
+				));
+			}
+
+			// TODO: Handle Uploaded Files.
+
+		} else {
+			$request = $request
+				->withParsedBody($this->expand($request->getParsedBody()))
+				->withUploadedFiles($this->expand($request->getUploadedFiles()))
+			;
+
+		}
+
+		return $request;
+	}
+
+
+	/**
+	 *
+	 */
+	public function processUrlEncoded(Request $request): Request
+	{
+		$parsed = array();
+
+		if (in_array($request->getMethod(), ['PUT', 'PATCH', 'DELETE'])) {
+			if (function_exists('mb_parse_str')) {
+				mb_parse_str($request->getBody()->getContents(), $parsed);
+			} else {
+				parse_str($request->getBody()->getContents(), $parsed);
+			}
+
+			$request = $request->withParsedBody($this->expand($parsed));
+
+		} else {
+			$request = $request
+				->withParsedBody($this->expand($request->getParsedBody()))
+			;
+
+		}
+
+		return $request;
+	}
+
+
+
+	/**
 	 * @param array<string, mixed> $inputs
 	 * @return array<string, mixed>
 	 */
-	protected function expand(Request $request, $inputs): array
+	protected function expand($inputs): array
 	{
 		$data = array();
 
